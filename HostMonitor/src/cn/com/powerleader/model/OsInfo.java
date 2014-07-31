0c0e73c8d61c2a43a725da6171cf066213f8f5f1
powerleader1 <powerleader1@192.168.2.21>
2014-07-31 17:16:12 +0800

    commit

--- HostMonitor/src/cn/com/powerleader/model/OsInfo.java
+++ HostMonitor/src/cn/com/powerleader/model/OsInfo.java
@@ -44,39 +44,11 @@ public class OsInfo implements java.io.Serializable {
 	private String ipmiPassword;
 	private double flowInRate;
 	private double flowOutRate;
-	public String getIpmiIp() {
-		return ipmiIp;
-	}
-
-	public void setIpmiIp(String ipmiIp) {
-		this.ipmiIp = ipmiIp;
-	}
-
-	public String getIpmiUserName() {
-		return ipmiUserName;
-	}
-
-	public void setIpmiUserName(String ipmiUserName) {
-		this.ipmiUserName = ipmiUserName;
-	}
-
-	public String getIpmiPassword() {
-		return ipmiPassword;
-	}
-
-	public void setIpmiPassword(String ipmiPassword) {
-		this.ipmiPassword = ipmiPassword;
-	}
-
-
-
+	
 	public OsInfo() {
+		super();
+		
 	}
-
-	public OsInfo(long osId) {
-		this.osId = osId;
-	}
-
 	public OsInfo(long osId, ServerManageInfo serverManageInfo,
 			String ipAddress1, Byte isVm, String osName, String osVersion,
 			String osBit, Byte osStatu, String osUser, String osPasswd,
@@ -106,254 +78,209 @@ public class OsInfo implements java.io.Serializable {
 		this.snmpUser = snmpUser;
 		this.snmpPasswd = snmpPasswd;
 	}
-
 	public long getOsId() {
-		return this.osId;
+		return osId;
 	}
-
-	
-	
-	public String getSysUptime() {
-		return sysUptime;
-	}
-
-	public void setSysUptime(String sysUptime) {
-		this.sysUptime = sysUptime;
-	}
-
-	public String getSysProcesses() {
-		return sysProcesses;
-	}
-
-	public void setSysProcesses(String sysProcesses) {
-		this.sysProcesses = sysProcesses;
-	}
-
-	public Float getCupUserate() {
-		return cupUserate;
-	}
-
-	public void setCupUserate(Float cupUserate) {
-		this.cupUserate = cupUserate;
-	}
-
-	public Float getMemUserate() {
-		return memUserate;
-	}
-
-	public void setMemUserate(Float memUserate) {
-		this.memUserate = memUserate;
-	}
-
-	public Float getHdUserate() {
-		return hdUserate;
-	}
-
-	public void setHdUserate(Float hdUserate) {
-		this.hdUserate = hdUserate;
-	}
-
 	public void setOsId(long osId) {
 		this.osId = osId;
 	}
-
 	public ServerManageInfo getServerManageInfo() {
-		return this.serverManageInfo;
+		return serverManageInfo;
 	}
-
 	public void setServerManageInfo(ServerManageInfo serverManageInfo) {
 		this.serverManageInfo = serverManageInfo;
 	}
-
 	public String getIpAddress1() {
-		return this.ipAddress1;
+		return ipAddress1;
 	}
-
 	public void setIpAddress1(String ipAddress1) {
 		this.ipAddress1 = ipAddress1;
 	}
-
 	public Byte getIsVm() {
-		return this.isVm;
+		return isVm;
 	}
-
 	public void setIsVm(Byte isVm) {
 		this.isVm = isVm;
 	}
-
 	public String getOsName() {
-		return this.osName;
+		return osName;
 	}
-
 	public void setOsName(String osName) {
 		this.osName = osName;
 	}
-
 	public String getOsVersion() {
-		return this.osVersion;
+		return osVersion;
 	}
-
 	public void setOsVersion(String osVersion) {
 		this.osVersion = osVersion;
 	}
-
 	public String getOsBit() {
-		return this.osBit;
+		return osBit;
 	}
-
 	public void setOsBit(String osBit) {
 		this.osBit = osBit;
 	}
-
 	public Byte getOsStatu() {
-		return this.osStatu;
+		return osStatu;
 	}
-
 	public void setOsStatu(Byte osStatu) {
 		this.osStatu = osStatu;
 	}
-
 	public String getOsUser() {
-		return this.osUser;
+		return osUser;
 	}
-
 	public void setOsUser(String osUser) {
 		this.osUser = osUser;
 	}
-
 	public String getOsPasswd() {
-		return this.osPasswd;
+		return osPasswd;
 	}
-
 	public void setOsPasswd(String osPasswd) {
 		this.osPasswd = osPasswd;
 	}
-
 	public String getIpAddress2() {
-		return this.ipAddress2;
+		return ipAddress2;
 	}
-
 	public void setIpAddress2(String ipAddress2) {
 		this.ipAddress2 = ipAddress2;
 	}
-
 	public String getIpAddress3() {
-		return this.ipAddress3;
+		return ipAddress3;
 	}
-
 	public void setIpAddress3(String ipAddress3) {
 		this.ipAddress3 = ipAddress3;
 	}
-
 	public String getIpAddress4() {
-		return this.ipAddress4;
+		return ipAddress4;
 	}
-
 	public void setIpAddress4(String ipAddress4) {
 		this.ipAddress4 = ipAddress4;
 	}
-
 	public Date getDataCreated() {
-		return this.dataCreated;
+		return dataCreated;
 	}
-
 	public void setDataCreated(Date dataCreated) {
 		this.dataCreated = dataCreated;
 	}
-
 	public Long getCreatedBy() {
-		return this.createdBy;
+		return createdBy;
 	}
-
 	public void setCreatedBy(Long createdBy) {
 		this.createdBy = createdBy;
 	}
-
 	public Date getDateModified() {
-		return this.dateModified;
+		return dateModified;
 	}
-
 	public void setDateModified(Date dateModified) {
 		this.dateModified = dateModified;
 	}
-
 	public Long getModifiedBy() {
-		return this.modifiedBy;
+		return modifiedBy;
 	}
-
 	public void setModifiedBy(Long modifiedBy) {
 		this.modifiedBy = modifiedBy;
 	}
-
 	public String getMemo() {
-		return this.memo;
+		return memo;
 	}
-
 	public void setMemo(String memo) {
 		this.memo = memo;
 	}
-
 	public String getManageName() {
-		return this.manageName;
+		return manageName;
 	}
-
 	public void setManageName(String manageName) {
 		this.manageName = manageName;
 	}
-
 	public String getSnmpUser() {
-		return this.snmpUser;
+		return snmpUser;
 	}
-
 	public void setSnmpUser(String snmpUser) {
 		this.snmpUser = snmpUser;
 	}
-
 	public String getSnmpPasswd() {
-		return this.snmpPasswd;
+		return snmpPasswd;
 	}
-
 	public void setSnmpPasswd(String snmpPasswd) {
 		this.snmpPasswd = snmpPasswd;
+	}
+	public Float getCupUserate() {
+		return cupUserate;
+	}
+	public void setCupUserate(Float cupUserate) {
+		this.cupUserate = cupUserate;
+	}
+	public Float getMemUserate() {
+		return memUserate;
+	}
+	public void setMemUserate(Float memUserate) {
+		this.memUserate = memUserate;
+	}
+	public Float getHdUserate() {
+		return hdUserate;
+	}
+	public void setHdUserate(Float hdUserate) {
+		this.hdUserate = hdUserate;
+	}
+	public String getSysUptime() {
+		return sysUptime;
+	}
+	public void setSysUptime(String sysUptime) {
+		this.sysUptime = sysUptime;
+	}
+	public String getSysProcesses() {
+		return sysProcesses;
+	}
+	public void setSysProcesses(String sysProcesses) {
+		this.sysProcesses = sysProcesses;
 	}
 	public String getNetFlowIn() {
 		return netFlowIn;
 	}
-
 	public void setNetFlowIn(String netFlowIn) {
 		this.netFlowIn = netFlowIn;
 	}
-
 	public String getNetFlowOut() {
 		return netFlowOut;
 	}
-
 	public void setNetFlowOut(String netFlowOut) {
 		this.netFlowOut = netFlowOut;
 	}
-
 	public Long getCurentTime() {
 		return curentTime;
 	}
-
 	public void setCurentTime(Long curentTime) {
 		this.curentTime = curentTime;
 	}
-
-	public double getFlowOutRate() {
-		return flowOutRate;
+	public String getIpmiIp() {
+		return ipmiIp;
 	}
-
-	public void setFlowOutRate(double flowOutRate) {
-		this.flowOutRate = flowOutRate;
+	public void setIpmiIp(String ipmiIp) {
+		this.ipmiIp = ipmiIp;
 	}
-
+	public String getIpmiUserName() {
+		return ipmiUserName;
+	}
+	public void setIpmiUserName(String ipmiUserName) {
+		this.ipmiUserName = ipmiUserName;
+	}
+	public String getIpmiPassword() {
+		return ipmiPassword;
+	}
+	public void setIpmiPassword(String ipmiPassword) {
+		this.ipmiPassword = ipmiPassword;
+	}
 	public double getFlowInRate() {
 		return flowInRate;
 	}
-
 	public void setFlowInRate(double flowInRate) {
 		this.flowInRate = flowInRate;
+	}
+	public double getFlowOutRate() {
+		return flowOutRate;
+	}
+	public void setFlowOutRate(double flowOutRate) {
+		this.flowOutRate = flowOutRate;
 	}
 
 
